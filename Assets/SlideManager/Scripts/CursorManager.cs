bbc2bb81baf1fa4ddedc98359cc3ead886f19982
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2022-11-28 19:15:28 +0100

    Add cursor Manager

---
+++ Assets/SlideManager/Scripts/CursorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Texture2D handCursor;
+    //Detect if the Cursor starts to pass over the GameObject
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        Cursor.SetCursor(handCursor, new Vector2(14, 6), CursorMode.Auto);
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
