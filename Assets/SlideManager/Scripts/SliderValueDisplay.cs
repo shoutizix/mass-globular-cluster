944b95fc2d68443a7f069e2f1db10686ed75a8ce
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2022-12-29 16:37:45 +0100

    Add sliders values

---
+++ Assets/SlideManager/Scripts/SliderValueDisplay.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider)), ExecuteInEditMode]
+public class SliderValueDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI valueTMP;
+
+    private Slider slider;
+    private float value;
+    private string format;
+
+    private void OnEnable()
+    {
+        if (!slider)
+        {
+            slider = GetComponent<Slider>();
+        }
+
+        format = slider.wholeNumbers ? "0" : "0.0";
+        UpdateSliderValue(slider.value);
+    }
+
+    public void UpdateSliderValue(float value)
+    {
+        if (valueTMP)
+        {
+            this.value = value;
+            valueTMP.text = this.value.ToString(format);
+        }
+    }
+}
